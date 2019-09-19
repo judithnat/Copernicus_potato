112f09bebf926e098566a695f33aa1f1271246dd
Alex Nathanail <alex@vectare.co.uk>
2019-09-19 11:06:15 +0100

    Docker initial

--- app/bestbarley/settings.py
+++ app/bestbarley/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vu262_&ci#fgjkub=34vzz%61-c6rnn@s7s6jby%cmp8#yin68'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
