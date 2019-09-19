a2cb42ef918e2ddc67c3837b551096170a5a2d4b
Alex Nathanail <alex@vectare.co.uk>
2019-09-19 11:37:43 +0100

    Allowed hosts

--- app/bestbarley/settings.py
+++ app/bestbarley/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', default='localhost|127.0.0.1').split('|')
+if DEBUG:
+  ALLOWED_HOSTS = '*'
 
 
 # Application definition
